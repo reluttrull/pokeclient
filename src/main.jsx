3161a11391dc1a18ae22577ce446fcaf3ba21211
Ryan Luttrull <reluttrull@gmail.com>
2025-10-22 12:58:40 -0500

    added gradient background, and temporarily removed strict mode for development testing

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>, // removed to test in dev, needs to be added back in prod
 )
