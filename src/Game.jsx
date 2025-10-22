22ca6f219a43cf5d684c5fdc5b8c1a0c54a15910
Ryan Luttrull <reluttrull@gmail.com>
2025-10-22 13:34:56 -0500

    they don't show up yet, but now able to attach cards to other cards (energies, evolutions).  still need to display energy indicators and handle precendence when it comes to evolutions

--- src/Game.jsx
+++ src/Game.jsx
@@ -16,9 +16,18 @@ const Game = ({gameStateCallback}) => {
     // update where card is placed
     console.log(`card ${data.num} moved to ${data.pos}`);
     var card = hand.concat(bench).concat(active).find(c => c.numberInDeck == data.num);
-    switch (data.pos) {
+    console.log(card);
+    placeCardInSpot(card, data.pos);
+  };
+
+  function placeCardInSpot(card, spot) {
+    switch (spot) {
       case 0:
-        setActive(card); // TODO: handle when another card is in the active spot?
+        if (active) { // this spot occupied, attach card instead
+          active.attachedCards.push(card);
+        } else { // spot empty, place card in spot
+          setActive(card);
+        }
         if (hand.includes(card)) setHand((hand) => hand.filter((c) => c.numberInDeck !== card.numberInDeck));
         else if (bench.includes(card)) setBench((bench) => bench.filter((c) => c.numberInDeck !== card.numberInDeck));
         break;
@@ -27,17 +36,27 @@ const Game = ({gameStateCallback}) => {
       case 3:
       case 4:
       case 5:
-        const nextBench = [
-          ...bench.slice(0, data.pos-1),
-          card,
-          ...bench.slice(data.pos-1)
-        ];
-        setBench(nextBench);
+        if (bench.length > spot && bench[spot]) { // this spot occupied, attach card instead
+          bench[spot].attachedCards.push(card);
+        } else { // spot empty, place card in spot
+          const nextBench = [
+            ...bench.slice(0, spot-1),
+            card,
+            ...bench.slice(spot-1)
+          ];
+          setBench(nextBench);
+        }
         if (hand.includes(card)) setHand((hand) => hand.filter((c) => c.numberInDeck !== card.numberInDeck));
         else if (active.numberInDeck == card.numberInDeck) setActive(null); 
         break;
       case 6:
         console.log('moving to discard');
+        // discard any attached cards first
+        card.attachedCards.forEach(function(c) {
+          setDiscard([c, ...discard]);
+          discardCard(c);
+        });
+        // then discard main card
         setDiscard([card, ...discard]);
         if (hand.includes(card)) setHand((hand) => hand.filter((c) => c.numberInDeck !== card.numberInDeck));
         else if (bench.includes(card)) setBench((bench) => bench.filter((c) => c.numberInDeck !== card.numberInDeck));
@@ -47,7 +66,7 @@ const Game = ({gameStateCallback}) => {
       default:
         break;
     }
-  };
+  }
 
   function getRandomCard() {
     let rand = Math.floor(Math.random() * baseset.length);
@@ -58,6 +77,7 @@ const Game = ({gameStateCallback}) => {
     fetch(`https://pokeserver20251017181703-ace0bbard6a0cfas.canadacentral-01.azurewebsites.net/game/drawcardfromdeck/${gameGuid.current}`)
     .then(response => response.json())
     .then(data => {
+      data.attachedCards = [];
       console.log('draw card', data);
       setHand([...hand, data]);
     })
@@ -107,7 +127,13 @@ const Game = ({gameStateCallback}) => {
             console.log('set game start');
         if (!data) throw "Game data empty!";
         if (data.gameGuid) gameGuid.current = data.gameGuid;
-        if (data.hand) setHand(data.hand);
+        if (data.hand) {
+          let expandedHand = data.hand.map(card => ({
+              ...card,
+              attachedCards: []
+          }));
+          setHand(expandedHand);
+        }
         if (data.mulligans) mulligans.current = data.mulligans;
         })
         .catch(error => console.error('Error fetching data:', error));
@@ -120,8 +146,8 @@ const Game = ({gameStateCallback}) => {
     <div style={{position: 'absolute', left: '700px', width: '200px'}}>active card = {active && active.name}</div>
     <div style={{position: 'absolute', top: '90px', left: '700px', width: '200px'}}># cards in hand = {hand && hand.length}</div>
     <div style={{position: 'absolute', top: '160px', left: '700px', width: '200px'}}># cards in bench = {bench && bench.length}</div>
-    <button onClick={drawTopCard} style={{position: 'absolute', top: '240px', left: '700px', width: '200px'}}>draw</button>
-    <button onClick={endGame} style={{position: 'absolute', top: '280px', left: '700px', width: '200px'}}>end game</button>
+    <button onClick={drawTopCard} id="draw-card-button">draw</button>
+    <button onClick={endGame} id="end-game-button">end game</button>
     <div id="user-active" className="card-target">
       {active && <Card key={active.numberInDeck} data={active} startOffset={0} positionCallback={cardCallback} />}
     </div>
