5c32840f4925cda7163baeb2aa91d268bdef9d59
Ryan Luttrull <reluttrull@gmail.com>
2025-11-01 10:23:04 -0500

    Added some subtle css glow to warn the user they may need to shuffle their deck Resolve #27

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -4,8 +4,15 @@ import './App.css';
 
 const Deck = ({displayNum, shuffleCallback, selectCallback}) => {
     const [isAnimated, setIsAnimated] = useState(false);
+    const [seenDeckOrder, setSeenDeckOrder] = useState(false);
+
+    function handleSelect (event) {
+        setSeenDeckOrder(true);
+        selectCallback();
+    }
 
     function handleShuffle (event) {
+        setSeenDeckOrder(false);
         setIsAnimated(true);
         shuffleCallback();
         setTimeout(() => setIsAnimated(false), 5000);
@@ -13,10 +20,10 @@ const Deck = ({displayNum, shuffleCallback, selectCallback}) => {
 
   return (
     <>
-    <div className="card-wrapper card-target" id="deck">
+    <div className="card-target" id="deck">
         <div className={isAnimated ? "card-list is-animated" : "card-list"}>
             <div style={{zIndex:6}}>{displayNum}</div>
-            <div className="card-list__item" data-card="0">
+            <div className={seenDeckOrder ? "card-list__item shuffle-reminder" : "card-list__item"} data-card="0">
                 <img src="/pokeclient/cardback.png" className="card-size" />
             </div>
             <div className="card-list__item" data-card="1">
@@ -26,7 +33,7 @@ const Deck = ({displayNum, shuffleCallback, selectCallback}) => {
                 <img src="/pokeclient/cardback.png" className="card-size" />
             </div>
         </div>
-        <button id="deck-select-button" className="button" onClick={selectCallback}>Select from deck</button>
+        <button id="deck-select-button" className="button" onClick={handleSelect}>Select from deck</button>
         <button className="button" onClick={handleShuffle}>Shuffle</button>
     </div>
     </>
